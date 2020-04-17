b8435e977d17e7eae3da91148a3327cc26222a52
Lucas Mariano <lucas.mariano.carmo@live.com>
2020-04-17 11:04:47 -0300

    update

--- Archi.DesignPatterns/Program.cs
+++ Archi.DesignPatterns/Program.cs
@@ -12,82 +12,26 @@ namespace Daily.DesignPatterns
     {
         static void Main()
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            WriteLine("------------------------");
             WriteLine("Choose:");
             WriteLine("1 - Creational Patterns");
             WriteLine("2 - Structural Patterns");
             WriteLine("3 - Behavioral Patterns");
-
             WriteLine("------------------------");
-            WriteLine("Creational Patterns");
-            WriteLine("------------------------");
-            WriteLine("1 - Abstract Factory");
-            WriteLine("2 - Method Factory");
-            WriteLine("3 - Singleton");
-            WriteLine("4 - Prototype");
-            WriteLine("------------------------");
-            WriteLine("Structural Patterns");
-            WriteLine("------------------------");
-            WriteLine("5 - Adapter");
-            WriteLine("6 - Facade");
-            WriteLine("7 - Composite");
-            WriteLine("8 - Bridge");
-            WriteLine("9 - Flyweight");
-            WriteLine("10 - Proxy");
-            WriteLine("------------------------");
-            WriteLine("Behavioral Patterns");
-            WriteLine("------------------------");
-            WriteLine("11 -  Command");
-            WriteLine("12 -  Mediator");
-            WriteLine("13 -  Observer");
-            WriteLine("14 - State");
-            WriteLine("15 - Strategy");
-            WriteLine("16 - Visitor");
-            WriteLine("------------------------");
-
             var option = Convert.ToString(ReadLine());
-
-            WriteLine("");
-            WriteLine("------------------------");
-            WriteLine("");
 
             switch (option)
             {
                 case "1":
-                
+                    CreationalPatterns.Call();
                     break;
                 case "2":
-                  
+                    BehaivioralPatterns.Call(); 
                     break;
                 case "3":
-                    ExecutorSingleton.Execute();
-                    break;
-                case "4":
-                    ExecutePrototype.Execute();
-                    break;
-                case "5":
-                   
-                    break;
-                case "6":
-                 
-                    break;
-                case "7":
-                    ExecutionCommand.Execute();
-                    break;
-                case "8":
-                    MediatorExecutor.Execute();
-                    break;
-                case "9":
-                    ExecuteObserver.Execute();
-                    break;
-                    
-                case "10":
-                
-                    break;
-                case "11":
-                   
-                    break;
-                case "12":
-                   
+                    StructuralPatterns.Call();
                     break;
             }
 
@@ -95,20 +39,122 @@ namespace Daily.DesignPatterns
             Clear();
             Main();
         }
-       
+
     }
     public static class CreationalPatterns
     {
-        
+        public static void Call()
+        {
+            WriteLine("Choose:");
+            WriteLine("\n");
+            WriteLine("Creational Patterns");
+            WriteLine("1 - Abstract Factory");
+            WriteLine("2 - Method Factory");
+            WriteLine("3 - Singleton");
+            WriteLine("4 - Prototype");
+            WriteLine("\n");
+
+            var option = Convert.ToString(ReadLine());
+
+            switch (option)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+                    ExecutorSingleton.Execute();
+                    break;
+                case "4":
+                    ExecutePrototype.Execute();
+                    break;
+            }
+        }
+
     }
     public static class BehaivioralPatterns
     {
+        public static void Call()
+        {
+            WriteLine("Choose:");
+            WriteLine("\n");
+            WriteLine("Structural Patterns");
+            WriteLine("1 - Adapter");
+            WriteLine("2 - Facade");
+            WriteLine("3 - Composite");
+            WriteLine("4 - Bridge");
+            WriteLine("5 - Flyweight");
+            WriteLine("6 - Proxy");
+            WriteLine("\n");
 
+            var option = Convert.ToString(ReadLine());
+
+            switch (option)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+                  
+                    break;
+                case "4":
+                 
+                    break;
+                case "5":
+                   
+                    break;
+                case "6":
+                   
+                    break;
+            }
+        }
     }
 
     public static class StructuralPatterns
     {
+        public static void Call()
+        {
+            WriteLine("Choose:");
+            WriteLine("\n");
+            WriteLine("Behavioral Patterns");
+            WriteLine("1 -  Command");
+            WriteLine("2 -  Mediator");
+            WriteLine("3 -  Observer");
+            WriteLine("4 - State");
+            WriteLine("5 - Strategy");
+            WriteLine("6 - Visitor");
+            WriteLine("\n");
 
+
+            var option = Convert.ToString(ReadLine());
+
+            switch (option)
+            {
+                case "1":
+                    ExecutionCommand.Execute();
+                    break;
+                case "2":
+                    MediatorExecutor.Execute();
+                    break;
+                case "3":
+                    ExecuteObserver.Execute();
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+
+                    break;
+                case "6":
+
+                    break;
+            }
+        }
     }
 
 
